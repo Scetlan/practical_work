d4cb5b90971e26c7e28a5d801e7657f1acfd7fcb
Scetlan <scetlan@MacBook-Air-Svetlana.local>
2023-11-08 20:23:58 +0300

    fix style

--- js/index.js
+++ js/index.js
@@ -1,35 +1,6 @@
-// swiper
-
-const swiper = new Swiper('.swiper', {
-    direction: 'horizontal',
-    loop: true,
-
-    pagination: {
-        el: '.swiper-pagination',
-    },
-
-    navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
-    },
-
-    scrollbar: {
-        el: '.swiper-scrollbar',
-    },
-    
-        breakpoints: {
-
-        320: {
-            slidesPerView: 2,
-            spaceBetween: 30,
-        },
-    },
-});
-
-
 const burgerMenu = document.querySelector('.mobil__burger-menu');
 const dropDown = document.querySelector('.drop-down');
-const menuClose = document.querySelector('.drop-down__burger-menu_close ');
+const menuClose = document.querySelector('.drop-down__burger-menu_close');
 
 burgerMenu.addEventListener('click', () => {
     dropDown.classList.add('drop-down--active');
@@ -41,15 +12,15 @@ menuClose.addEventListener('click', () => {
 
 // событие на кнопку 'показать еще'
 const brandItems = document.querySelectorAll('.brand__item');
-const brandBtn = document.querySelector('.brand__btn');
+const brandBtn = document.querySelector('.more-buttom');
 const brandAll = document.querySelector('.brand__all');
-const brandSvgBtn = document.querySelector('.brand__svg-btn');
+const brandSvgBtn = document.querySelector('.more-buttom__svg');
 
 brandBtn.addEventListener('click', () => {
     brandItems.forEach(item => {
         item.classList.add('brand__item--avtive');
     });
-    brandSvgBtn.classList.add('brand__svg-btn--transform');
+    brandSvgBtn.classList.add('more-buttom__svg--transform');
     if (brandAll.textContent === 'Показать все') {
         brandAll.textContent = 'Скрыть';
     } else {
@@ -57,6 +28,33 @@ brandBtn.addEventListener('click', () => {
         brandItems.forEach(item => {
             item.classList.remove('brand__item--avtive');
         });
-        brandSvgBtn.classList.remove('brand__svg-btn--transform');
+        brandSvgBtn.classList.remove('more-buttom__svg--transform');
     }
+});
+
+
+// swiper
+
+const swiper = new Swiper('.swiper', {
+    direction: 'horizontal',
+    loop: true,
+
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+    },
+
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+
+    breakpoints: {
+
+        320: {
+            slidesPerView: 2,
+            spaceBetween: 16,
+        },
+    },
+
 });
